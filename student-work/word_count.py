b759b097c6409b20cc4583b1220a46d225f053c9
nathanj727 <nathanj.burchfield@gmail.com>
2017-07-12 09:00:25 -0700

    Delete word_count.py

--- student-work/word_count.py
+++
@@ -1,8 +0,0 @@
-from collections import Counter
-
-
-def word_count(text):
-    def replace_non_alpha(char):
-        return char.lower() if char.isalnum() else ' '
-    text = ''.join(replace_non_alpha(c) for c in text)
-    return Counter(text.split())
