dc5b85f3e15f209c957e3d7fbc716c34b5338ff3
Andrew Telleria <atelleria08@gmail.com>
2017-07-17 20:00:08 -0700

    csv_coffee

--- student-work/andrew_k_telleria/week_2/day_1/working_with_csvs/open_tech_csvs.py
+++ student-work/andrew_k_telleria/week_2/day_1/working_with_csvs/open_tech_csvs.py
@@ -2,7 +2,33 @@
 # either reference them by uning a relative path
 # Ex. "../../../../../shared-resources/<csv_file_name>.csv"
 # or if you prefer you can make a copy and move the files into this directory
-# just please try to remember not to commit the csv files when you're saving 
-# your work. 
+# just please try to remember not to commit the csv files when you're saving
+# your work.
 
 
+import csv
+import collections
+# import geo_distance
+
+def csv_coffee():
+    with open("coffee.csv") as coffee_csv:
+        my_reader = csv.reader(coffee_csv)
+
+        # calling next will reutrn the next row in the file which is often the row of headers
+        # header = next(my_reader)
+
+        for row in my_reader:
+            # print(row)
+            pass
+
+        [print(row) for row in my_reader if row[0] == 'Flat White']
+
+def csv_airport():
+    with open("airports.csv") as airports_csv:
+        airport_reader = csv.reader(airports_csv)
+        for row in csv.reader(airports_csv):
+            if row[3] == 'England':
+                print(row[1])
+
+csv_coffee()
+csv_airport()
