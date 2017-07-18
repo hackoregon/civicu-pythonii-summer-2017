675721396054e53eb3700ab43ce4a83b726e7bfb
Jacob Janak <jakeathotmail@gmail.com>
2017-07-17 20:31:04 -0700

    Done with csv stuff

--- student-work/JacobJanak/week_2/day_1/working_with_csvs/open_tech_csvs.py
+++ student-work/JacobJanak/week_2/day_1/working_with_csvs/open_tech_csvs.py
@@ -1,8 +1,53 @@
-# to use the csvs in the class's shared-resoruces directory you'll need to
-# either reference them by uning a relative path
-# Ex. "../../../../../shared-resources/<csv_file_name>.csv"
-# or if you prefer you can make a copy and move the files into this directory
-# just please try to remember not to commit the csv files when you're saving 
-# your work. 
+import csv
+import geo_distance
 
+def find_airports(city):
+    with open("airports.csv") as file:
+        reader = csv.reader(file)
 
+        header = next(reader)
+        print(header)
+
+        for row in reader:
+            if row[2] == city:
+                print(row)
+
+find_airports('New York')
+
+def distance(city1, city2):
+    with open("airports.csv") as file:
+        reader = csv.reader(file)
+
+        for row in reader:
+            if row[2] == city1:
+                city1_lat = row[6]
+                city1_long = row[7]
+            elif row[2] == city2:
+                city2_lat = row[6]
+                city2_long = row[7]
+
+        try:
+            city1_lat
+        except NameError:
+            print("Oops, couldn't find that city.")
+            return
+
+        try:
+            city2_lat
+        except NameError:
+            print("Oops, couldn't find that city.")
+            return
+
+        city1_lat = float(city1_lat)
+        city1_long = float(city1_long)
+        city2_lat = float(city2_lat)
+        city2_long = float(city2_long)
+
+        result = geo_distance.distance(city1_lat, city1_long, city2_lat, city2_long)
+        row = [city1, city2, result]
+
+    with open('result.csv', 'a') as file:
+        writer = csv.writer(file)
+        writer.writerow(row)
+
+distance('Boston', 'Paris')
