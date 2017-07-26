4417caeb683a66805d6ee7ba3ce5b682eb6db571
Ashley Riehl <ashleyriehl@node-tfv.pool-125-25.dynamic.totbb.net>
2017-07-26 13:55:05 -0400

    add to csv function

--- student-work/ashley_riehl/week_2/day_1/working_with_csvs/open_tech_csvs.py
+++ student-work/ashley_riehl/week_2/day_1/working_with_csvs/open_tech_csvs.py
@@ -6,6 +6,7 @@
 # your work.
 
 import csv
+from geo_distance import distance
 
 def csv_coffee():
     with open("coffee.csv") as coffee_csv:
@@ -28,11 +29,49 @@ def open_airports():
             if row[3] == 'Latvia':
                 print(row)
 
-open_airports()
-#dict of airports and lat, long
+# open_airports()
 
-def get_route_distance()
+#lat1, lat2, long1, long2
+def get_route_distance():
     #Make latitude Dictionary
-    with open("")
-    latitudes = {id:lat for }
-    #Make longitude Dictionary
+    with open("airports.csv") as airports_csv:
+        airport_reader = csv.reader(airports_csv)
+        airport_header = next(airport_reader)
+        latitudes = {row[0]:row[6] for row in airport_reader}
+    with open("airports.csv") as airports_csv:
+        airport_reader = csv.reader(airports_csv)
+        longitudes = {row[0]:row[7] for row in airport_reader}
+    with open("routes.csv") as routes_csv:
+        route_reader = csv.reader(routes_csv)
+        route_header = next(route_reader)
+        no_id = 0
+        errors = []
+        for row in route_reader:
+            from_air = row[3]
+            to_air = row[5]
+            #Get origin lat, longs
+            try:
+                from_lat, from_long = latitudes[from_air], longitudes[from_air]
+                print(from_lat, from_long)
+            except KeyError:
+                no_id += 1
+                if from_air not in errors:
+                    errors.append(from_air)
+            #Get destination lat, longs
+            try:
+                to_lat, to_long = latitudes[to_air], longitudes[to_air]
+                print(to_lat, to_long)
+            except KeyError:
+                no_id += 1
+                if to_air not in errors:
+                    errors.append(from_air)
+            route_distance = distance(from_lat, from_long, to_lat, to_long)
+            print(route_distance)
+            # new_row = [from_air, lat1, long1, to_air, lat2, long2, distance]
+            # with open("combined_file.csv", "a") as combined:
+            #     combined.write()
+
+        print("Records that don't match:", str(no_id))
+        print("Errors: ",errors)
+
+get_route_distance()
