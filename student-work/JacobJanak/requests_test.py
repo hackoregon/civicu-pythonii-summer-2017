5647fd114d082433a6f9f2b94997aa3deb03b279
Jacob Janak <jakeathotmail@gmail.com>
2017-07-26 19:52:34 -0700

    added year 2017

--- student-work/JacobJanak/requests_test.py
+++ student-work/JacobJanak/requests_test.py
@@ -1,16 +1,21 @@
 import json
 import requests
 
-def how_many_homeless():
-    total_count = 0
-    for i in range(2001, 2015, 2):
-        url = 'http://service.civicpdx.org/homeless/ethnicity/?format=json&page=1&year={}'.format(i)
-        r = requests.get(url)
-        data = json.loads(r.content.decode())
-        if data:
-            for i in data:
-                total_count += i['count']
-    return total_count
 
-print('{} total homeless people in Portland over the years'.format(how_many_homeless()))
+def how_many_homeless():
+    count = 0
+    for year in range(2007, 2017, 2): # increment by 2 since there's only odd years
+        url = f'http://service.civicpdx.org/homeless/ethnicity/?format=json&page=1&year={year}'
+        r = requests.get(url)
+        if r.status_code == 200: # make sure your internet is working and url's haven't changed
+            data = json.loads(r.content.decode())
+            if data:
+                count += sum([entry['count'] for entry in data])
+
+    return count
+
+homeless_count = how_many_homeless()
+print(f'{homeless_count} total homeless people in Portland over the years')
+
+# 18,210 is the answer
 
