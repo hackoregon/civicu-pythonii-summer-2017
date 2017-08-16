97e0fe736cb5b49660fb850b39a61830003cc142
Hobson Lane <github@totalgood.com>
2017-08-16 09:51:14 -0700

    add notebooks and nbconvert files

---
+++ lessons/09-Pandas/upload-file-with-POST.py
@@ -0,0 +1,20 @@
+
+# coding: utf-8
+
+# In[2]:
+
+
+import requests
+import os
+
+base_dir = os.path.join(os.getenv('HOME'), 'Pictures')
+url = 'http://localhost:8000/api/'
+filename = 'barlow-rd-2014-a-095_30343888245_o.jpg'
+with open(os.path.join(base_dir, 'bear', filename), 'rb') as fin:
+    print(fin.name)
+    POST_data = {'caption': 'posted bear'}
+    files = {'file': (filename, fin),
+             'file_name': filename}
+    resp = requests.post(url, data=POST_data, files=files)
+    print(resp)
+
