6bc6ea336bb71c22eb515b9a489010e01c2dff8b
Cassandra Delieto <cassandradelieto@gmail.com>
2017-08-29 19:27:31 -0700

    exercism solutions

--- student-work/cassandradelieto/Exercism_solutions/python/meetup/meetup.py
+++ student-work/cassandradelieto/Exercism_solutions/python/meetup/meetup.py
@@ -8,22 +8,23 @@ def meetup_day(year, month, weekday, descriptor):
     For example, if given "First Monday of January 2017", the correct meetup date is 2017/1/2
     expressed as date(YYYY,MM,DD)
     '''
+
     DESCRIPTORS = {
-                   1: "1st",
-                   2: "2nd",
-                   3: "3rd",
-                   4: "4th",
-                   5: "5th",
-                   teenth: "teenth",
-                   last: "last",
+        '1st': 1,
+        '2nd': 8,
+        '3rd': 15,
+        '4th': 22,
+        '5th': 29,
+        'teenth': 13,
                    }
 
-    start = date(year, month, day)
-    day =
-    offset = list(day_name).index(day) - start.weekday()
-    return start +
+    start = date(year, month, DESCRIPTORS.get(descriptor, last(year, month)))
+    offset = list(day_name).index(weekday) - start.weekday()
+    return start + timedelta(days = offset % 7)
+
+def last(year, month):
+    return([25,22 + int(isleap(year))] + [25,24,25,24,25]*2)[month-1]
 
 
 #list(calendar.day_name) = list of all day names
 #days.index(day) = get the index of the selected day
-
