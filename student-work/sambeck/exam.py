d4ff66f51d61cd6b5a9a7da0c1a4bb13f13464a5
notsambeck <thesamuelbeck@gmail.com>
2017-09-03 14:28:20 -0700

    made a non-interactive mode of exam for testing; added test_answers.pkl

--- student-work/sambeck/exam.py
+++ student-work/sambeck/exam.py
@@ -5,18 +5,21 @@ import pickle
 import json
 
 
-def read_exam(filepath='../../lessons/shared-resources/exam.csv'):
+def read_exam(filepath='../../lessons/shared-resources/exam.csv', inter=True):
     '''
-read_exam takes the path to this exam and returns 3 lists as
-a truple with elements:
-1. question numbers
-2. question strings
-3. list of incorrect answers
->>> type(read_exam())
-tuple
+read_exam is an interactive test_taking program.
+a list of correct answers is saved to a pickle; load it with make_answers()
+
+ARGS:
+  filepath: string: path to test .csv, default is exam.csv in shared-resources
+  inter: bool: whether test is interactive. default is true, false for testing
+
+RETURNS:
+  3-tuple: (question_numbers, question_text, incorrect_answers)
     '''
+
     if filepath.startswith('http'):
-        return 'maybe just downlod this file'
+        return 'maybe just download the file?'
 
     with open(filepath) as f:
         question_numbers = []
@@ -29,6 +32,10 @@ tuple
         header = [str(e).strip("\"") for e in dirtyheader]
         print(header)
         print()
+        if inter:
+            print('enter 1 for true answers. enter 0 (or _) for false answers')
+        else:
+            print('exam running in auto-mode')
 
         # while True:
         for i in range(11):
@@ -36,18 +43,24 @@ tuple
                 dirtyline = f.readline().strip().split(',')
                 line = [str(e).strip("\"") for e in dirtyline]
                 # print(line)
-                question_numbers.append(line[0])
+                question_numbers.append(int(line[0]))
                 question_text.append(line[1])
                 possibly_random_letters.append(line[2])
 
-                print(line[1])
-                print('coded answer?', line[2])
-                print('answers', line[3:])
+                print()
+                print(line[0], '=> ', line[1])
+                print('coded answer(s):', line[2])
+                print('answer choices:', line[3:])
                 my_answers.append('')
                 incorrect_answers.append([])
                 for i in range(3, len(line)):
-                    answer_is_correct = input(line[i])
-                    if not answer_is_correct:  # because we want incorrect
+                    # inter is true by default, false when run in exam_tests
+                    if inter:
+                        answer_is_correct = input(line[i])
+                    else:
+                        answer_is_correct = i % 2  # simulate input for test
+
+                    if not answer_is_correct:  # we want incorrect answers
                         incorrect_answers[-1].append(line[i])
                     else:
                         my_answers[-1] = my_answers[-1] + header[i]
@@ -56,16 +69,38 @@ tuple
                 print('out of lines')
                 break
 
-    with open('answers.pkl', 'wb') as outf:
+    if inter:
+        outfile = 'answers.pkl'
+    else:
+        outfile = 'test_answers.pkl'  # so real answers are not saved over
+
+    with open(outfile, 'wb') as f:
+        # dump questions and answers to pickle
         for lst in [question_numbers, question_text,
                     incorrect_answers, my_answers]:
-            pickle.dump(lst, outf)
+            pickle.dump(lst, f)
 
-    return (question_numbers, question_text, incorrect_answers), my_answers
+    # return only the tuple
+    return (question_numbers, question_text, incorrect_answers)
 
 
-def load_all():
-    with open('answers.pkl', 'rb') as f:
+def make_answers(inter=True):
+    '''
+loads variables previously stored by read_exam
+
+ARGS:
+  inter: bool: interactive mode, true by default, false for test_make_answers
+
+RETURNS:
+  QUESTIONS, QUESTION_TEXTS, ANSWERS, MY_ANSWERS: lists spec'd by exam
+  MY_ANSWERS_JS: string: json of answers set by input
+'''
+    if inter:
+        infile = 'answers.pkl'
+    else:
+        infile = 'test_answers.pkl'
+
+    with open(infile, 'rb') as f:
         QUESTIONS = pickle.load(f)
         QUESTION_TEXTS = pickle.load(f)
         ANSWERS = pickle.load(f)
@@ -77,14 +112,15 @@ def load_all():
             l.append({QUESTIONS[i]: MY_ANSWERS[i]})
         MY_ANSWERS_JS = json.dumps(l)
 
-        # print(json.loads(MY_ANSWERS_JS))
-
         return QUESTIONS, QUESTION_TEXTS, ANSWERS, MY_ANSWERS, MY_ANSWERS_JS
 
 
 if __name__ == '__main__':
     try:
-        QUESTIONS, QUESTION_TEXTS, ANSWERS, MY_ANSWERS, MY_ANSWERS_JS = load_all()
+        QUESTIONS, QUESTION_TEXTS, ANSWERS,\
+            MY_ANSWERS, MY_ANSWERS_JS = make_answers()
     except IOError:
-        print('pickle is missing, so you should do test yourself.')
+        print('pickled answers do not exist; you need to take the test.')
         read_exam()
+        QUESTIONS, QUESTION_TEXTS, ANSWERS,\
+            MY_ANSWERS, MY_ANSWERS_JS = make_answers()
